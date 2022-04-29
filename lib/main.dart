6091f183f94eb5d62dab1a1b756831cbe9baa38c
aungkyawlin <aungkyawlinmon@gmail.com>
2022-04-29 21:55:40 +0700

    Start Project

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const appTitle = "Form Validation";
+    
+    return MaterialApp(
+      title: appTitle,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(appTitle),
+        ),
+        body: CustomForm(),
+      ),
+    );
+  }
+}
+
+class CustomForm extends StatefulWidget {
+  const CustomForm({Key? key}) : super(key: key);
+
+  @override
+  State<CustomForm> createState() => _CustomFormState();
+}
+
+class _CustomFormState extends State<CustomForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
